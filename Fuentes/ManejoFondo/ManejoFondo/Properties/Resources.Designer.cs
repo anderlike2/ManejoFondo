13d01419110a20228ec44d4f9c9a971eeb963a9d
anderlike2 <anderlike2@hotmail.co>
2019-06-03 12:51:47 -0500

    Ajustes generales

--- Fuentes/ManejoFondo/ManejoFondo/Properties/Resources.Designer.cs
+++ Fuentes/ManejoFondo/ManejoFondo/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ManejoFondo.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Cargando {
+            get {
+                object obj = ResourceManager.GetObject("Cargando", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cerrarSesion {
             get {
                 object obj = ResourceManager.GetObject("cerrarSesion", resourceCulture);
